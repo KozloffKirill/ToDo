395c22e8fd6687d11668ad592bb598349e3922a8
Kirill Kozlov <kirillkozlove39@gmail.com>
2021-11-21 20:44:47 +0300

    new team component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { TaskBoardComponent } from './components/task-board/task-board.component
 import { TaskComponent } from './components/task/task.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { TeamComponent } from './components/team/team.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
     TasksComponent,
     TaskBoardComponent,
     TaskComponent,
-    TaskListComponent
+    TaskListComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
