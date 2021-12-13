0ca666c135fc315ac51c9f3a2f8c670f7f84f4c4
Kirill Kozlov <kirillkozlove39@gmail.com>
2021-12-13 23:16:48 +0300

    developed new-employee component

--- src/app/components/new-employee/new-employee.component.ts
+++ src/app/components/new-employee/new-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { IEmployee, Position } from 'src/app/models/employees';
+import { addEmployee } from 'src/app/store/employees/employees.actions';
 
 @Component({
   selector: 'app-new-employee',
@@ -8,13 +11,55 @@ import { Component, OnInit } from '@angular/core';
 export class NewEmployeeComponent implements OnInit {
 
   public employeeName: string = '';
+  public employeePosition: string = 'Junior Developer';
 
-  constructor() { }
+  public positions: string[] = [
+    'Junior Developer',
+    'Middle Developer',
+    'Senior Developer',
+    'Project Manager',
+    'Tester',
+    'Analyst'
+  ];
+
+
+  constructor(
+    private _store: Store
+  ) { }
 
   ngOnInit(): void {
   }
 
   public addEmployee(): void {
+    let position: Position;
+    switch (this.employeePosition) {
+      case 'Junior Developer':
+        position = Position.JuniorDeveloper
+        break;
+      case 'Middle Developer':
+        position = Position.MiddleDeveloper
+        break;
+      case 'Senior Developer':
+        position = Position.SeniorDeveloper
+        break;
+      case 'Project Manager':
+        position = Position.ProjectManager
+        break;
+      case 'Tester':
+        position = Position.Tester
+        break;
+      case 'Analyst':
+        position = Position.Analyst
+        break;
+      default:
+        position = Position.JuniorDeveloper
+        break;
+    }
+    const newEmployee: IEmployee = {
+      name: this.employeeName,
+      position: position
+    }
+    this._store.dispatch(addEmployee({ employee: newEmployee }));
     this.employeeName = '';
   }
 
