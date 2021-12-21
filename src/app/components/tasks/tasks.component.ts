3af9022107c1f9e85e118d0dda1131f7a9af8927
Kirill Kozlov <kirillkozlove39@gmail.com>
2021-12-22 01:00:57 +0300

    weather

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -15,6 +15,9 @@ export class TasksComponent implements OnInit, OnDestroy {
 
   public user!: IUser;
 
+  public temperature: any = 0;
+  public humidity: any = 0;
+
   constructor(
     private _store: Store
   ) { }
@@ -22,6 +25,21 @@ export class TasksComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this._sub = this._store.select(selectUser).subscribe(
       (user) => this.user = user
+    );
+
+    let apiKey = "8a38ff8566e4ff7f4fe96ac3e14ce28f";
+    let city = "Saint Petersburg";
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`;
+    let promise = fetch(url).then(
+      (weather) => {
+        return weather.json();
+      }
+    ).then(
+      (weather) => {
+        console.log(weather);
+        this.temperature = weather.main.temp;
+        this.humidity = weather.main.humidity;
+      }
     );
   }
 
