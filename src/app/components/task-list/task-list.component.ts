08a10948677914ec4fea14a3d0bd3aa54726ee71
Kirill Kozlov <kirillkozlove39@gmail.com>
2021-12-21 22:58:45 +0300

    trash empty fix

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,5 +1,5 @@
 import { CdkDragDrop } from '@angular/cdk/drag-drop';
-import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewChecked, ChangeDetectorRef, Component, ElementRef, Input, OnChanges, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { ITask, TaskType } from 'src/app/models/tasks';
@@ -11,7 +11,10 @@ import { selectTypeTasks } from 'src/app/store/tasks/tasks.selectors';
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
-export class TaskListComponent implements OnChanges, OnInit, OnDestroy {
+export class TaskListComponent implements OnChanges, OnInit, OnDestroy, AfterViewChecked {
+
+  @ViewChild('list') taskList!: ElementRef;
+  public listHeight!: number;
 
   @Input() public taskType!: TaskType;
 
@@ -28,7 +31,8 @@ export class TaskListComponent implements OnChanges, OnInit, OnDestroy {
   private _tasksSub!: Subscription;
 
   constructor(
-    private _store: Store
+    private _store: Store,
+    private _cdr: ChangeDetectorRef
   ) { }
 
   public deleteTasks() {
@@ -46,12 +50,17 @@ export class TaskListComponent implements OnChanges, OnInit, OnDestroy {
     );
   }
 
+  ngAfterViewChecked(): void {
+    this.listHeight = this.taskList.nativeElement.offsetHeight;
+    this._cdr.detectChanges();
+  }
+
   ngOnDestroy(): void {
     this._tasksSub.unsubscribe();
   }
 
   public drop(event: CdkDragDrop<ITask[]>) {
-    if(event.item.dropContainer.data[event.previousIndex].status == this.taskType && event.previousIndex == event.currentIndex) return;
+    if (event.item.dropContainer.data[event.previousIndex].status == this.taskType && event.previousIndex == event.currentIndex) return;
     this._store.dispatch(editTaskOrder(
       {
         task: event.item.dropContainer.data[event.previousIndex],
